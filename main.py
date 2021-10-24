591bad7d0bc7670e1c6e471a32a56e1e9970f3fa
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2021-10-24 00:11:30 -0400

    least constrained vlaue

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ class Node:
         self.dominio = []
         self.value=None
         self.conflict_List = []
+        self.neighbours=[]
         
+
+def forward_checking(node, value):
+    node.dominio.append(value)
+    for neighbour in node.neighbours:
+        neighbour.dominio.remove(value)
+    return node
+
+
 
 
 def asignation_complete(list_Nodes):
@@ -23,22 +32,20 @@ def most_Constrained_Value(list_Nodes):
 def least_Contrained_Value(node,list_Nodes):
     allconsistentvalues= []
     for value in node.dominio:
-        temp_x=copy(x)
-        forward_checking(Xi,v)
+        temp_list_Nodes=copy.deepcopy(list_Nodes)
+        forward_checking(node,value)
         consistent_values=0
-        foreach Xe in Xi.neighbours{
-            consisten_values+=len(Xe.dominio)
-        }
-        allconsistentvalues.push(v,consistentvalues)
-        x=temp_x
-    }
-    return allconsistentValues.pop
+        for neighbour in node.neighbours:
+            consistent_values+=len(neighbour.dominio)   
+        heapq.heappush(allconsistentvalues,(consistent_values,value))     
+        list_Nodes=temp_list_Nodes
+    return allconsistentvalues[0]
 
 def backTrack(list_Nodes,weight,list_dominios):
     if asignation_complete(list_Nodes): #todos los nodos estan asignados
         return list_Nodes
     node_to_assign_value=most_Constrained_Value(list_Nodes) #Escoger variable no asignada
-    node_to_assign_value=change_Dominio(node_to_assign_value,least_Contrained_Value(node_to_assign_value,x))
+    node_to_assign_value=change_Dominio(node_to_assign_value,least_Contrained_Value(node_to_assign_value,list_dominios))
     foreach v in dominios
     if(asginacion_es_correcta(node_to_assign_value,v)==0)//Invalido
         continue
